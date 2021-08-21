14cdcb7fc282fa03de3e5c3d15d89c07d2d4cd9e
Ameer Mousavi <xerxes1718@gmail.com>
2021-08-21 06:59:14 +0430

    ready to release

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:*'
+  });
   await app.listen(3000);
   if (module.hot) {
     module.hot.accept();
